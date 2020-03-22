ce3ea4a190fe9991c1ad20a27735973256656cf7
IvanKMK <kumeyko@hotmail.com>
2020-03-22 21:25:42 +0100

    clear git cache

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/ivankmk/Side_projects/12.ivan_is_learning/ivan-is-learning/courses/devman/frontend/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
